c938bce9567ee17eca148beb2347d79cb52c69ec
Otto <otto@Ottos-MacBook-Pro.local>
2024-08-20 00:48:36 +0200

    chhanges

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ import { lusitana } from "@/src/styles/fonts";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col justify-center items-center">
-      <div className="md:w-2/5 md:px-20">
-        <div className="flex h-20 shrink-0 justify-center rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="md:w-2/5 md:px-20 px-4">
+        <div className="flex h-20 justify-center rounded-lg bg-blue-500 p-4 md:h-52">
           <PageLogo />
         </div>
         <div className="mt-4">
