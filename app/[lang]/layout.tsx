1f454d9a746032695449a9f7a01bb1314b66a110
ottobonilla95 <ottobonilla.dev@gmail.com>
2024-12-07 07:23:30 -0500

    chahnges

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -120,6 +120,23 @@ export default async function RootLayout({
           data-domain="trackmyspend.co"
           src="https://datafa.st/js/script.js"
         ></script>
+
+        {process.env.NODE_ENV === "production" && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(h,o,t,j,a,r){
+                  h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                  h._hjSettings={hjid:5234021,hjsv:6};
+                  a=o.getElementsByTagName('head')[0];
+                  r=o.createElement('script');r.async=1;
+                  r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                  a.appendChild(r);
+              })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            `,
+            }}
+          />
+        )}
       </head>
       <body className={`${bricolageGrotesque.className} antialiased`}>
         {children}
