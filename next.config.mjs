83a953368afa3bc0f3a88501892bbb4d0da91421
ottobonilla95 <ottobonilla.dev@gmail.com>
2025-03-17 12:22:19 -0500

    cahgnes

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:lang",
-        destination: "/:lang/landing-page",
-        permanent: false, // Set to true if this is a permanent redirect (301), false for temporary (307)
-      },
-    ];
-  },
+  async redirects() {},
 };
 export default nextConfig;
